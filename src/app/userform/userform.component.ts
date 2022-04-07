753729225dc4264a746a281af32b2995da108082
ngactive99 <ngactive99@gmail.com>
2022-04-07 09:22:40 +0530

    display the list of users

--- src/app/userform/userform.component.ts
+++ src/app/userform/userform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
 import { UserService } from '../user.service';
 
 @Component({
@@ -9,16 +10,21 @@ import { UserService } from '../user.service';
 export class UserformComponent implements OnInit {
   user = {
     name: '',
-    age:10,
+    age: 0,
     gender: "Male"
   }
+  users = []
   save() {
     // this.userService.createUser(this.user);
     const observable = this.userService.createUser(this.user);
-    observable.subscribe(response => {
+    observable.subscribe((response: any)  => { // sucess handler
       console.log(response);
+      this.users.push(response);
       
-    })
+    },
+    function (error) {
+      alert("Something went wrong please try again!")
+    });
 
 
     // console.log( "hello mr / mrs " + this.user.name + " you are " + this.user.age + " years old ");
